bb88b2ab1c0ffc85643c719dd4a2cbbba46c025f
Arif Yaman <yamanarif05@gmail.com>
2025-09-03 00:46:45 +0300

    ortalamaya bağlı lkalma geçme durumu belirleme

--- app/src/main/java/yeni/klasör/App.java
+++ app/src/main/java/yeni/klasör/App.java
@@ -5,12 +5,15 @@ public class App {
     int matematik , fizik , kimya , turkce , tarih , muzik;
         Scanner ders_notu = new Scanner(System.in);
 
-        matematik = ders_notu.nextInt();
         System.out.println("Matematik notun:");
+        matematik = ders_notu.nextInt();
+        
+        
         System.out.println("fizik notun:");
         fizik = ders_notu.nextInt();
 
         System.out.println("kimya notun:");
+        
         kimya = ders_notu.nextInt();
 
         System.out.println("türkçe notun:");
@@ -24,6 +27,9 @@ public class App {
         
         double ort;
         ort = (matematik + fizik + kimya + turkce + tarih + muzik) / 6.0;
+        String durum = (ort > 60) ? "geçtiniz" : "kaldınız";
         System.out.println("ders ortalaman:" + ort);
+        System.out.println(durum);
+        ders_notu.close();
     }
 }
