b1d7a67d3beac64b1a03f84ec6843854f7223ef8
Arif Yaman <yamanarif05@gmail.com>
2025-09-02 03:15:33 +0300

    Sadeleştirildi.

--- app/src/main/java/yeni/klasör/App.java
+++ app/src/main/java/yeni/klasör/App.java
@@ -3,32 +3,27 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
     int matematik , fizik , kimya , turkce , tarih , muzik;
-        System.out.println("Matematik notun:");
         Scanner ders_notu = new Scanner(System.in);
-        int m_n = ders_notu.nextInt();
 
+        matematik = ders_notu.nextInt();
+        System.out.println("Matematik notun:");
         System.out.println("fizik notun:");
-        Scanner ders_notu2 = new Scanner(System.in);
-        int f_n = ders_notu.nextInt();
+        fizik = ders_notu.nextInt();
 
         System.out.println("kimya notun:");
-        Scanner ders_notu3 = new Scanner(System.in);
-        int k_n = ders_notu.nextInt();
+        kimya = ders_notu.nextInt();
 
         System.out.println("türkçe notun:");
-        Scanner ders_notu4 = new Scanner(System.in);
-        int t_n = ders_notu.nextInt();
+        turkce = ders_notu.nextInt();
         
         System.out.println("tarih notun:");
-        Scanner ders_notu5 = new Scanner(System.in);
-        int tarih_n = ders_notu.nextInt();
+        tarih = ders_notu.nextInt();
 
         System.out.println("müzik notun:");
-        Scanner ders_notu6 = new Scanner(System.in);
-        int müzik_n = ders_notu.nextInt();
+        muzik = ders_notu.nextInt();
         
-        int ort;
-        ort = (m_n + f_n + k_n + t_n + tarih_n + müzik_n) / 6;
+        double ort;
+        ort = (matematik + fizik + kimya + turkce + tarih + muzik) / 6.0;
         System.out.println("ders ortalaman:" + ort);
     }
 }
